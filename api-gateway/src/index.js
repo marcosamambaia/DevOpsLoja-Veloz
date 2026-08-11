cd289978b7d964ee9769fad4b80a78009adc4510
Marco Samambaia <marcosamambaia@gmail.com>
2026-08-11 20:17:42 -0300

    Teste pipeline CI/CD

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -40,3 +40,4 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`API Gateway rodando na porta ${port}`);
 });
+//teste*****************************************************
